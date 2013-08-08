6fb2adf211854536dc73e90e0806eb01a78671b5
swhustla <fdkelly@gmail.com>
2013-08-08 10:25:41 +0000

    Added jetstrap files and updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var string = fs.readFileSync('screen-f64af35f23.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(string);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
